cb0310854d2b1ad057332754a422c1964f6912d2
Niklas Schönberg <niklas@uplink.fi>
2025-05-05 14:33:20 +0300

    fix: Name chagne

--- jcore-lohko.php
+++ jcore-lohko.php
@@ -1,11 +1,12 @@
 <?php
 /**
- * Plugin Name:       Lohko
- * Description:       Example block scaffolded with Create Block tool.
+ * Plugin Name:       JCORE Lohko
+ * Description:       Template for default blocks used in JCORE 3.
  * Version:           0.1.0
  * Requires at least: 6.7
- * Requires PHP:      7.4
- * Author:            The WordPress Contributors
+ * Requires PHP:      8.2
+ * Author:            J&Co Digital
+ * Author URI:        https://jco.fi
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       lohko
