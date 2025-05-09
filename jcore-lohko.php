672f9fa5373960b3d9190b1c88d3bab175190923
Niklas Schönberg <niklas@uplink.fi>
2025-05-09 10:44:10 +0300

    0.2.2

--- jcore-lohko.php
+++ jcore-lohko.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       JCORE Lohko
  * Description:       Template for default blocks used in JCORE 3.
- * Version: 0.2.1
+ * Version: 0.2.2
  * Requires at least: 6.7
  * Requires PHP:      8.2
  * Author:            J&Co Digital
